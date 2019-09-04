7b12a43f79ac04b54755226f2e93be0529bfb5ef
Mohammad Fikri <fikrim30@gmail.com>
2019-09-04 19:17:58 +0700

    Create api route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/users/list', 'UserController@index')->name('users.index');
+Route::post('/users/create', 'UserController@create')->name('users.create');
+Route::get('/users/{user}/manager', 'UserController@show_manager')->name('users.manager');
+Route::put('/users/{user}/manager', 'UserController@assign_manager')->name('users.assign_manager');
+Route::get('/users/{user}/subordinates', 'UserController@show_subordinates')->name('users.subordinates');
+Route::put('/users/{user}', 'UserController@update')->name('users.update');
+Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
