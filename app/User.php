ffee078dca9290e3faf658339b119b5bbb769b06
Mohammad Fikri <fikrim30@gmail.com>
2019-09-04 19:16:27 +0700

    Create user table db migration and model

--- app/User.php
+++ app/User.php
@@ -2,38 +2,27 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
+    public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $with = ['friends', 'manager', 'subordinates'];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function manager(){
+        return $this->belongsTo('App\User', 'manager_id');
+    }
+
+    public function subordinates(){
+        return $this->hasMany('App\User', 'manager_id');
+    }
+
+    public function friends(){
+        return $this->belongsToMany('App\User', 'friendship', 'user_id', 'friend_id')->withPivot('approved');
+    }
 }
--- database/migrations/2019_09_04_120640_create_friendship_table.php
+++ database/migrations/2019_09_04_120640_create_friendship_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFriendshipTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->integer('manager_id')->nullable();
+        Schema::create('friendship', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('friend_id');
+            $table->tinyInteger('approved');
         });
     }
 
@@ -27,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('friendship');
     }
 }
