086c52021b2d0d73cd064bf4a94f56a5f1a3f351
Mohammad Fikri <fikrim30@gmail.com>
2019-09-05 07:31:54 +0700

    Add routes for manage friendship between users

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
@@ -24,3 +23,8 @@ Route::put('/users/{user}/manager', 'UserController@assign_manager')->name('user
 Route::get('/users/{user}/subordinates', 'UserController@show_subordinates')->name('users.subordinates');
 Route::put('/users/{user}', 'UserController@update')->name('users.update');
 Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/friends/{user}/list', 'FriendshipController@list_friend')->name('friends.list');
+Route::get('/friends/{user}/request/list', 'FriendshipController@list_friend_request')->name('friend_requests.list');
+Route::post('/friends/request/create', 'FriendshipController@create_friend_request')->name('friend_requests.create');
+Route::put('/friends/{user}/request/approve', 'FriendshipController@approve_friend_request')->name('friend_requests.approve');
