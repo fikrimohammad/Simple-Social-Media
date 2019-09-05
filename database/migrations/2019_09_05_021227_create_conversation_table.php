def35c0aed9c1f427c709e7a3ddeea9681dbec4e
Mohammad Fikri <fikrim30@gmail.com>
2019-09-05 14:19:44 +0700

    Add migrations for `conversation` and `message` table

---
+++ database/migrations/2019_09_05_021227_create_conversation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversation', function (Blueprint $table) {
+            $table->integer('host_id');
+            $table->integer('recipient_id');
+            $table->string('conversation_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversation');
+    }
+}
---
+++ database/migrations/2019_09_05_050517_create_message_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('conversation_id')->nullable();
+            $table->integer('sender_id')->nullable();
+            $table->integer('recipient_id')->nullable();
+            $table->string('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
