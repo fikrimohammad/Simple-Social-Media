4bcd952c58aae7408ef734aba452e6e7d44f7579
Mohammad Fikri <fikrim30@gmail.com>
2019-09-06 19:29:44 +0700

    Config logger and proxy middleware

--- config/logging.php
+++ config/logging.php
@@ -36,16 +36,24 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
-            'ignore_exceptions' => false,
+            'channels' => ['single'],
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
             'level' => 'debug',
         ],
+        // 'stack' => [
+        //     'driver' => 'stack',
+        //     'channels' => ['daily'],
+        //     'ignore_exceptions' => false,
+        // ],
 
+        // 'single' => [
+        //     'driver' => 'single',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => 'debug',
+        // ],
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
